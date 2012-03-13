0e1e2c933eafc63287d9abdf2620c212b8b6df10
Andrew Anderson <gimlilongbow@gmail.com>
2012-03-13 14:32:24 -0400

    Adding CampMinder failed login page template and associated code.

--- javascripts/theBeesKnees.js
+++ javascripts/theBeesKnees.js
@@ -7,6 +7,12 @@
 */ 
 
 /** 
+* Global variables.
+*/
+
+var urlParams = {};
+
+/** 
 * Initial document.ready call.
 * All default page load actions here.
 */
@@ -14,10 +20,34 @@ $(function() {
 	$('#id').on('click', clickHandler); // Preferred click listener format as of jQuery 1.7.
 });
 
+
+/***************
+ * Click handlers.
+ ***************/
+
 /**
 * Standard JSDoc format.
 * Function definition with brief description.
 */
 clickHandler = function(event) {
 	event.stopPropagation(); // Use this or return false; to cancel default event.
+}
+
+
+/***************
+ * General functions.
+ ***************/
+
+/**
+ * Loads URL parameters into an array for local usage.
+ */
+loadUrlParams = function () {
+	var e,
+		a = /\+/g,  // Regex for replacing addition symbol with a space
+		r = /([^&=]+)=?([^&]*)/g,
+		d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+		q = window.location.search.substring(1);
+
+	while (e = r.exec(q))
+		urlParams[d(e[1])] = d(e[2]);
 }
